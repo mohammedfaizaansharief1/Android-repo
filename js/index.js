1cd4d6ee57ae4288b4360957c02c30b448ab9965
MohammedFaizaanSharief <mohdfaizaansharief@gmail.com>
2023-11-26 14:48:46 +0530

    small_changes

---
+++ js/index.js
@@ -0,0 +1,7 @@
+for(let i=0;i<2;i++){
+        let row = document.createElement("tr")
+            for(let j=0;j<2;j++){
+                    let cell = document.createElement("td")
+                            let celltext = 
+                                }
+                              }
